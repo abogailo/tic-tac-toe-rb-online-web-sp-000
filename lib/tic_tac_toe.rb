845edc997576932aba36f3446b3aeb85402133e6
Abagail Hudson <abagailanne@gmail.com>
2019-04-30 23:43:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,8 +17,12 @@ WIN_COMBINATIONS = [
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
- def move(board, location, current_player = "X")
-  board[location.to_i-1] = current_player
+def input_to_index(input)
+    index = input.to_i - 1
+end
+
+ def move(board, index, boardCharacter = "X")
+  board[index] = boardCharacter
 end
 
  def position_taken?(board, location)
@@ -31,9 +35,10 @@ end
 
  def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  if valid_move?(board, input)
-    move(board, input, current_player(board))
+  input = gets.chomp
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
   else
     turn(board)
   end
