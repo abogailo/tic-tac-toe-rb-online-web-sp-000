0dba357e29ca8e508bd388eda33d2d8c6d9e3426
Abagail Hudson <abagailanne@gmail.com>
2019-04-30 23:30:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,9 +66,9 @@ end
     position_3 = board[win_index_3]
 
      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      true
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      true
     else
       false
     end
